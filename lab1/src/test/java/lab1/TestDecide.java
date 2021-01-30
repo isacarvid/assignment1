f8c7befec369779ad19145cc68c11db9fc3b0e18
Eva Despinoy <despinoy@kth.se>
2021-01-30 15:09:37 +0100

    Changed LIC5 so its uses compere and added working test for it

--- lab1/src/test/java/lab1/TestDecide.java
+++ lab1/src/test/java/lab1/TestDecide.java
@@ -2,34 +2,36 @@ package lab1;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.jupiter.api.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-class TestDecide {
-
+public class TestDecide {
     // here should probably put arrays which test like all the lics
     @Before
     public void setUp() {
-
+        System.out.println("setup");
     }
 
     @Test
     //Returns true if there exists at least two consecutive
     //data pts (xi yi) and (xj yj) where xj - xi < 0
     public void LIC5test() {
-        double[] LIC5Truex = new double[] {1, 5, 3.5};
+        double[] LIC5Truex = new double[] {3, 2, 1};
         double[] LIC5Truey = new double[] {0, -2, 9};
-        double[] LIC5Falsex = new double[] {7, 5, 3};
-        double[] LIC5Falsey = new double[] {0, 1, -5};
-
         Decide decide = new Decide();
+        decide.numpoints = 3;
         decide.coordinatex = LIC5Truex;
         decide.coordinatey = LIC5Truey;
         assertTrue(decide.LIC5(decide.parameters));
 
-        decide = new Decide();
-        decide.coordinatex = LIC5Falsex;
-        decide.coordinatey = LIC5Falsey;
-        assertFalse(decide.LIC5(decide.parameters));
+        double[] LIC5Falsex = new double[] {1, 2, 3};
+        double[] LIC5Falsey = new double[] {1, 2, 5};
+        Decide decide2 = new Decide();
+        decide2.numpoints = 3;
+        decide2.coordinatex = LIC5Falsex;
+        decide2.coordinatey = LIC5Falsey;
+        assertFalse(decide2.LIC5(decide2.parameters));
     }
 }
