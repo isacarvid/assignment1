bf473917e581e5117622fbf19ad189d58a7328de
Eva Despinoy <despinoy@kth.se>
2021-01-30 12:42:39 +0100

    added test for LIC5, still not working

--- lab1/src/test/java/lab1/TestDecide.java
+++ lab1/src/test/java/lab1/TestDecide.java
@@ -1,5 +1,35 @@
 package lab1;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
 
-public class TestDecide {
+import static org.junit.Assert.*;
 
+class TestDecide {
+
+    // here should probably put arrays which test like all the lics
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    //Returns true if there exists at least two consecutive
+    //data pts (xi yi) and (xj yj) where xj - xi < 0
+    public void LIC5test() {
+        double[] LIC5Truex = new double[] {1, 5, 3.5};
+        double[] LIC5Truey = new double[] {0, -2, 9};
+        double[] LIC5Falsex = new double[] {7, 5, 3};
+        double[] LIC5Falsey = new double[] {0, 1, -5};
+
+        Decide decide = new Decide();
+        decide.coordinatex = LIC5Truex;
+        decide.coordinatey = LIC5Truey;
+        assertTrue(decide.LIC5(decide.parameters));
+
+        decide = new Decide();
+        decide.coordinatex = LIC5Falsex;
+        decide.coordinatey = LIC5Falsey;
+        assertFalse(decide.LIC5(decide.parameters));
+    }
 }
